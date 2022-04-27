2a86e8cf215b71b86190c9092552305142894ceb
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-04-27 15:36:51 +0200

    Initial Commit: SetUp Basic Project

---
+++ src/test/java/ALP/KBEWarehouse/KbeWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ALP.KBEWarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
